4c6df1937b88f2f28983487eeaaf83c4318ec94e
Sébastien D <sebastien.delahaye.contact@gmail.com>
2023-07-11 14:38:15 +0200

    ✨ Creation of the api account

--- migrations/Version20230711123038.php
+++ migrations/Version20230711123038.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20230710123703 extends AbstractMigration
+final class Version20230711123038 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,6 +20,7 @@ final class Version20230710123703 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE api_account (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(180) NOT NULL, roles LONGTEXT NOT NULL COMMENT \'(DC2Type:json)\', password VARCHAR(255) NOT NULL, created_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', updated_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', UNIQUE INDEX UNIQ_900822C3E7927C74 (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE customer (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(50) NOT NULL, created_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', updated_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE product (id INT AUTO_INCREMENT NOT NULL, brand VARCHAR(255) NOT NULL, model VARCHAR(255) NOT NULL, system VARCHAR(255) NOT NULL, storage INT NOT NULL, price DOUBLE PRECISION NOT NULL, stock INT NOT NULL, created_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', updated_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, customer_id INT NOT NULL, email VARCHAR(255) NOT NULL, username VARCHAR(255) NOT NULL, password VARCHAR(255) NOT NULL, first_name VARCHAR(50) NOT NULL, last_name VARCHAR(50) NOT NULL, created_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', updated_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', INDEX IDX_8D93D6499395C3F3 (customer_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
@@ -30,6 +31,7 @@ final class Version20230710123703 extends AbstractMigration
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE user DROP FOREIGN KEY FK_8D93D6499395C3F3');
+        $this->addSql('DROP TABLE api_account');
         $this->addSql('DROP TABLE customer');
         $this->addSql('DROP TABLE product');
         $this->addSql('DROP TABLE user');
---
+++ src/DataFixtures/ApiAccountFixtures.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\ApiAccount;
+use App\Entity\Customer;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
+use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
+
+class ApiAccountFixtures extends Fixture implements DependentFixtureInterface
+{
+    public function getDependencies()
+    {
+        return [
+            CustomerFixtures::class,
+        ];
+    }
+
+    public function load(ObjectManager $manager): void
+    {
+        $faker = Factory::create();
+
+        $customers = $manager->getRepository(Customer::class)->findAll();
+        foreach($customers as $customer) {
+            $email = $customer->getName()."@business.com";
+            $role = ["ROLE_USER"];
+            $password = $faker->password();
+            $createdAt = $customer->getCreatedAt();
+            $updatedAt = $customer->getUpdatedAt();
+
+            $apiAccount = new ApiAccount();
+            $apiAccount->setEmail($email);
+            $apiAccount->setRoles($role);
+            $apiAccount->setPassword($password);
+            $apiAccount->setCreatedAt($createdAt);
+            $apiAccount->setUpdatedAt($updatedAt);
+
+            $manager->persist($apiAccount);
+        }
+
+        $manager->flush();
+    }
+}
